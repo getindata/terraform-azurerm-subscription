be01675c8d979cda63f01d1da0ad7e5322bdb23d
Jakub Igla <jakub.igla@getindata.com>
2022-09-22 14:14:45 +0200

    feat: Add time provider for default value of start_date for budget

--- locals.tf
+++ locals.tf
@@ -3,6 +3,8 @@ locals {
 
   consumption_budget_defaults = {
     time_grain = "Monthly"
+    consumption_budget_start_date = (length(time_static.consumption_budget_start_date) == 0 ? null :
+    formatdate("YYYY-MM-01'T'00:00:00Z", one(time_static.consumption_budget_start_date[*].rfc3339)))
     notifications = {
       contact_emails = var.default_consumption_budget_notification_emails
       operator       = "EqualTo"
